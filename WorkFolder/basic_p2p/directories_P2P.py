4fe60e4350f2608cdeb779ccdd880342d7b96183
rubabi <rubabishmam@gmail.com>
2023-11-05 21:12:55 +0100

    Added rubab as user

--- WorkFolder/basic_p2p/directories_P2P.py
+++ WorkFolder/basic_p2p/directories_P2P.py
@@ -4,7 +4,7 @@ import getpass
 current_user = getpass.getuser()
 JK = "jakob"
 OH = "olehermanimset"
-RI = "rubabishmam"
+RI = "rubab"
 
 def directory(folder):
     
@@ -20,6 +20,13 @@ def directory(folder):
             return '/Users/olehermanimset/Library/CloudStorage/OneDrive-NTNU/9. Semester/Project Thesis/IFO25---Project-Thesis/WorkFolder/basic_p2p/data/test_case/'
         elif folder == "results":
             return '/Users/olehermanimset/Library/CloudStorage/OneDrive-NTNU/9. Semester/Project Thesis/IFO25---Project-Thesis/WorkFolder/basic_p2p/results/test_case/'
+        else:
+            print('Invalid directory input')
+    elif current_user == RI:
+        if folder == "data":
+            return 'C:\Users\rubab\Desktop\NTNU\Semester\11. semester\TIØ4550 - Investering, finans, økonomistyring, fordypningsprosjekt\KodingIFØ25 MASTER\IFO25---Project-Thesis\WorkFolder\basic_p2p\data\test_case'
+        elif folder == "results":
+            return 'C:\Users\rubab\Desktop\NTNU\Semester\11. semester\TIØ4550 - Investering, finans, økonomistyring, fordypningsprosjekt\KodingIFØ25 MASTER\IFO25---Project-Thesis\WorkFolder\basic_p2p\results\test_case'
         else:
             print('Invalid directory input')
     else:
