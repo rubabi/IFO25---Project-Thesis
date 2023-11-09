287216c521d1cc018cdc7e318ed29b3d520515da
Ole Herman Imset <ohimset@gmail.com>
2023-11-09 14:54:44 +0100

    .

--- WorkFolder/basic_p2p/model_components_P2P.py
+++ WorkFolder/basic_p2p/model_components_P2P.py
@@ -85,7 +85,7 @@ def model_p2p(data):
     model.FFR_discharging_capacity = Constraint(model.T, model.H_bat, rule=FFR_discharging_capacity)
     
     def FFR_capacity_sum(model,t):
-        return sum(model.R_FFR_charge[t,h] + model.R_FFR_discharge[t,h] for h in model.H_bat) >= model.Z_FFR #Problem related to half hour time steps   
+        return sum(model.R_FFR_charge[t,h] + model.R_FFR_discharge[t,h] for h in model.H_bat) >= model.Z_FFR 
     model.FFR_capacity_sum = Constraint(model.T_FFR, rule=FFR_capacity_sum)
     #---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
