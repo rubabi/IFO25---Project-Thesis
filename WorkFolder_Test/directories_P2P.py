788a0d81297e9acf74f57eec6fe7f9572ccc29b1
Ole Herman Imset <ohimset@gmail.com>
2023-11-17 09:18:33 +0100

    Remove unnecessary files from repository

--- WorkFolder_Test/directories_P2P.py
+++ WorkFolder_Test/directories_P2P.py
@@ -10,21 +10,21 @@ def directory(folder):
     
     if current_user == JK:
         if folder == "Test case":
-            return "C:/Users/jakob/Documents/Masteroppgave/IFO25---Project-Thesis/Workfolder_Test/data/Test case/"
+            return "C:/Users/jakob/Documents/Masteroppgave/IFO25---Project-Thesis/Workfolder_Test/data/"
         elif folder == "results":
             return "C:/Users/jakob/Documents/Masteroppgave/IFO25---Project-Thesis/Workfolder_Test/results/"
         else:
             print('Invalid directory input')
     elif current_user == OH:
         if folder == "Test case":
-            return '/Users/olehermanimset/Library/CloudStorage/OneDrive-NTNU/9. Semester/Project Thesis/IFO25---Project-Thesis/Workfolder_Test/data/Test case/'
+            return '/Users/olehermanimset/Library/CloudStorage/OneDrive-NTNU/9. Semester/Project Thesis/IFO25---Project-Thesis/Workfolder_Test/data/'
         elif folder == "results":
             return '/Users/olehermanimset/Library/CloudStorage/OneDrive-NTNU/9. Semester/Project Thesis/IFO25---Project-Thesis/Workfolder_Test/results/'
         else:
             print('Invalid directory input')
     elif current_user == RI:
         if folder == "Test case":
-            return 'C:/Users/rubab/Desktop/NTNU/Semester/11Semester/TIO4550/KodingIFO25/IFO25---Project-Thesis/Workfolder_Test/data/Test case/'
+            return 'C:/Users/rubab/Desktop/NTNU/Semester/11Semester/TIO4550/KodingIFO25/IFO25---Project-Thesis/Workfolder_Test/data/'
         elif folder == "results":
             return 'C:/Users/rubab/Desktop/NTNU/Semester/11Semester/TIO4550/KodingIFO25/IFO25---Project-Thesis/Workfolder_Test/results/'
         else:
