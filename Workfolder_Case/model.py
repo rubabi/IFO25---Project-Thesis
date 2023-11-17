9ec158831488c52251adc6fc3c31ba4d251ee45c
Ole Herman Imset <ohimset@gmail.com>
2023-11-17 12:07:39 +0100

    Refactor print statements in model.py

--- Workfolder_Case/model.py
+++ Workfolder_Case/model.py
@@ -78,12 +78,10 @@ if continuous_switch:
     P2P_savings = savings[2]
     FFR_savings = savings[3]
     
-    print(f'No P2P, batteries or PV production (base case): {round(no_savings,2)} NOK')
-    print()
+    print(f'\nNo P2P, batteries or PV production (base case): {round(no_savings,2)} NOK')
     print(f'The total bill reduction is: {round(float(bill_reduction)*100,2)}%')
     print(f'P2P savings: {round(P2P_savings/no_savings*100,2)}%')
-    print(f'FFR savings: {round(FFR_savings/no_savings*100,2)}%')
-    print()
+    print(f'FFR savings: {round(FFR_savings/no_savings*100,2)}%\n')
 
 #--------------------------------------------------------------------------------------------------------------------------------------
 
