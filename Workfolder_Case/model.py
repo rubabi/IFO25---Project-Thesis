c819dc11eeb0e84484c67d2c43553da8e9c45578
Ole Herman Imset <ohimset@gmail.com>
2023-11-20 11:41:03 +0100

    Fix FFR_type validation in model.py

--- Workfolder_Case/model.py
+++ Workfolder_Case/model.py
@@ -26,7 +26,7 @@ end_date = '2021-7-01' # Between 2021-4-02 and 2021-7-01, end date is not includ
 
 #$ 'Flex', 'Profil' or 'No FFR'
 FFR_type = 'No FFR'
-if FFR_type != 'No FFR' or FFR_type != 'Flex' or FFR_type != 'Profil':
+if FFR_type != 'No FFR' and FFR_type != 'Flex' and FFR_type != 'Profil':
     raise ValueError('FFR_type must be either "Flex", "Profil" or "No FFR"') 
 
 #$ System component switches (booleans)
